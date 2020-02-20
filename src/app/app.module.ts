83573461d8430eef02ea5e6861c61acc139a8e27
cole <cole.d.vick@gmail.com>
2020-02-20 13:15:00 -0500

    add initial project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimeTableComponent } from './time-table/time-table.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeTableComponent,
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule
