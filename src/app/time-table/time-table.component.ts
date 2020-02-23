b772f3432d8b2577ff42036b590620c5cefe33f8
cole <cole.d.vick@gmail.com>
2020-02-23 17:53:17 -0500

    setting up station/schedule subscription in time-table

--- src/app/time-table/time-table.component.ts
+++ src/app/time-table/time-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+import { Subscription, BehaviorSubject } from 'rxjs';
+import { Station } from '../station.model';
 
 @Component({
   selector: 'app-time-table',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeTableComponent implements OnInit {
 
-  constructor() { }
+  station: Station;
+  currentStationUpdate: BehaviorSubject<Station>;
+  private stationSub: Subscription;
+
+  constructor(public searchService: SearchService) { }
 
   ngOnInit(): void {
+    this.station = this.searchService.getStation();
+    this.stationSub = this.searchService.getStationSubscription()
+      .subscribe((stationData: {station: Station}) => {
+        this.station = stationData.station;
+        this.currentStationUpdate.next(this.station);
+      });
   }
-
 }
