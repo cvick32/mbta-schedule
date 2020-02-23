10ccfbf3df32cb1e2ef4bf63a4f9583fb99876e8
cole <cole.d.vick@gmail.com>
2020-02-23 18:09:23 -0500

    add preliminary timetable html and change title

--- src/app/angular-mat.module.ts
+++ src/app/angular-mat.module.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatTableModule
   ]
 })
 export class AngularMaterialModule {}
